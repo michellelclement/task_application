0fdcaaaf2575c582f70204fe6031ed8f3cf5bdfc
Michelle Clement <michellelclement@gmail.com>
2020-08-30 10:59:29 +0000

    Connect flask to mongodb ready to creat templates

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 #Create new instance of Flask app and store in variable app
 app = Flask(__name__) 
 
+#Link to MongoDB Database
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:R00tUser@myfirstcluster.visoz.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+#Create an instance of PyMongo
+mongo = PyMongo(app)
+
 @app.route('/') #/ refers to defult route
-def hello():
-    return 'Hello World... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 #Set up IP address and port number so it knows where and how to run application
 if __name__ == '__main__':
